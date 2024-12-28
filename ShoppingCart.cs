1b1864a3b21addc1f0b9cd3759624100f4b20174
Vatrushka0258 <fangelinak@gmail.com>
2024-12-28 10:38:59 +1000

    Добавьте файлы проекта.

---
+++ ShoppingCart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class ShoppingCart
+    {
+        public List<Product> products;
+        public ShoppingCart()
+        {
+            products = new List<Product>();
+        }
+        public void AddProduct(Product products)
+        {
+            
+        }
+
+        public decimal GetTotalPrice()
+        {
+            decimal totalPrice = products.Sum(p => p.Price);
+
+            if (products.Count >= 5)
+            {
+                totalPrice += 0.9m; // скидка 10%
+            }
+            else if (products.Count <= 5)
+            {
+                totalPrice += 1.1m; // добовляю скидку к общей стоимости
+            }
+            return totalPrice;
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cart = new ShoppingCart();
+            cart.AddProduct(new Product() { Name = "Product 1", Price = 10 });
+            cart.AddProduct(new Product() { Name = "Product 2", Price = 20 });
+            cart.AddProduct(new Product() { Name = "Product 3", Price = 30 });
+            cart.AddProduct(new Product() { Name = "Product 4", Price = 40 });
+            cart.AddProduct(new Product() { Name = "Product 5", Price = 50 });
+
+            decimal totalPrice = cart.GetTotalPrice();
+
+            Assert.AreEqual(155, totalPrice);
+        }
+        [TestMethod]
+       
+        public void TestPriceIncrease()
+        {
+            var cart = new ShoppingCart();
+            cart.AddProduct(new Product() { Name = "Product 1", Price = 10 });
+            cart.AddProduct(new Product() { Name = "Product 2", Price = 20 });
+            decimal totalPrice = cart.GetTotalPrice();
+
+            Assert.AreEqual(33, totalPrice); // общая стоимость с добавлением 10% 
+        }
+    }
+}
